a9e50b0ba899dfaf8f3f767219dc3040d46aa8e5
Naveenravi07 <gamerkidnav@gmail.com>
2023-09-28 18:47:31 +0530

    main route

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 
 import express from "express";
-import userRouter  from './controllers/user'
-import {connectDb} from "./database/database"
+import userRouter from './controllers/user'
+import { connectDb } from "./database/database"
 import cors from 'cors'
 
 const PORT = 42069;
 
 const app = express();
 
-app.use(cors({origin:"*"}))
-app.use(express.urlencoded({extended:true}));
+app.use(cors({ origin: "*" }))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/users",userRouter)
+app.get('/', (_, res) => {
+    res.send('200')
+})
+
+app.use("/users", userRouter)
 
 app.listen(PORT, async () => {
     console.log("Server started in port " + PORT)
