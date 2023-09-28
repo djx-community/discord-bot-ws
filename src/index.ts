df5db8598ed9bd87f3d55bc5d3e2ed7a762a31d1
Naveenravi07 <gamerkidnav@gmail.com>
2023-09-28 16:03:05 +0530

    cors added

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@
 import express from "express";
 import userRouter  from './controllers/user'
 import {connectDb} from "./database/database"
+import cors from 'cors'
 
 const PORT = 42069;
+
 const app = express();
 
+app.use(cors({origin:"*"}))
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
