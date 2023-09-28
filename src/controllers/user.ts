d4b466c04d43733bc8bd4b1a1102261bf0ae2279
Naveenravi07 <gamerkidnav@gmail.com>
2023-09-28 16:56:00 +0530

    bug fix

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -7,19 +7,22 @@ type user = {
     fullname: string,
     phone: number,
     discordId: string,
-    github:string,
-    linkedin:string,
-    tech:string[]
+    github: string,
+    linkedin: string,
+    tech: string[]
 }
 
 userRouter.post('/register', async function(req, res) {
     try {
-        let body : user = req.body
+        let body: user = req.body
+        if(!body.discordId){    
+           return res.status(400).json({ message: "discord id required"})
+        }
         let prevcheck = await userModel.findOne({ discordId: body.discordId });
         if (prevcheck) {
             return res.status(409).json({ message: "user already exists with this discordId" })
         }
-        let doc = await new userModel({...body}).save()
+        let doc = await new userModel({ ...body }).save()
         res.status(200).json(doc)
     } catch (err) {
         res.status(400).json({ message: "error occured", data: err })
